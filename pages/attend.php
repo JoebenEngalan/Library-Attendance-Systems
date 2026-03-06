567efc871eba58b3531b4771df2d1bb9b810cf52
JoebenEngalan <trone1993@gmail.com>
2026-03-06 09:57:34 +0800

    Changes on Attend

--- pages/attend.php
+++ pages/attend.php
@@ -33,7 +33,7 @@ $queryStudent->execute();
 $student = $queryStudent->fetch(PDO::FETCH_OBJ);
 
 if (!$student) {
-    $_SESSION['message'] = "<span style='color:red;'>❌ Student ID not found. Please register.</span>";
+    $_SESSION['message'] = "<span style='color:red;'>❌ Student ID not found. Please register. Contact the administrator.</span>";
     header("Location: ../index.php");
     exit();
 }
